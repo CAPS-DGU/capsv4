1c2fdc65ed40a2c7977ab67914c5cb124917d433
Jinsoo Yoon <floreo1242@gmail.com>
2024-07-31 00:05:54 +0900

    Initial Commit - Spring Boot Project

---
+++ backend/src/main/java/kr/dgucaps/capsv4/Capsv4Application.java
@@ -0,0 +1,13 @@
+package kr.dgucaps.capsv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capsv4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Capsv4Application.class, args);
+	}
+
+}
