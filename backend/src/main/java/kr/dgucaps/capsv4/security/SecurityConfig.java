c5be50ca09f97c12547dd8d6c716a3e7d65ff1ca
Jinsoo Yoon <floreo1242@gmail.com>
2024-08-16 02:49:29 +0900

    Feat: Implement JWT-based authentication and authorization system
    - Added `JwtToken` DTO for response handling.
    - Configured secret key in `application.yml` for JWT encryption and decryption.
    - Created `JwtTokenProvider` to handle JWT creation, encryption, decryption, and validation.
    - Implemented JwtAuthenticationFilter for authentication using userID and password.
    - Configured Spring Security in `SecurityConfig` to use JWT for authentication.

---
+++ backend/src/main/java/kr/dgucaps/capsv4/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package kr.dgucaps.capsv4.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(HttpBasicConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorization ->
+                        authorization
+                                .requestMatchers(HttpMethod.POST, "/user", "/user/login", "/user/findUserId").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/user/**/validate").permitAll()
+                                .requestMatchers("/test").hasRole("MEMBER")
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
