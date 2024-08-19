91d6b041e63dacb03912bef76725cdf32635f8da
Jinsoo Yoon <floreo1242@gmail.com>
2024-08-19 21:56:27 +0900

    Feat: 회원가입 입력 검증 추가

--- backend/src/main/java/kr/dgucaps/capsv4/dto/request/CreateUserRequest.java
+++ backend/src/main/java/kr/dgucaps/capsv4/dto/request/CreateUserRequest.java
@@ -8,12 +8,18 @@ import lombok.Data;
 public class CreateUserRequest {
 
     @NotBlank
+    @Size(min = 4, max = 12)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "영문 대소문자와 숫자만 사용할 수 있습니다")
     private String userId;
 
     @NotBlank
+    @Size(min = 8, max = 20)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "영문 대소문자와 숫자만 사용할 수 있습니다")
+    @Pattern(regexp = "^(?=.*[0-9])", message = "숫자 1개 이상 포함해야 합니다")
     private String password;
 
     @NotBlank
+    @Pattern(regexp = "^[\\uAC00-\\uD7AF]{2,4}$", message = "2 ~ 4 자리의 정상적인 한글 이름을 사용해야 합니다")
     private String name;
 
     @NotNull
