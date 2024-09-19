6fb8761b9e8ca963fc05a199fbc9c524aea86659
Jinsoo Yoon <floreo1242@gmail.com>
2024-09-19 22:26:52 +0900

    Feat: 이벤트 목록 조회 시 fetch join 하도록 설정

--- backend/src/main/java/kr/dgucaps/capsv4/repository/EventRepository.java
+++ backend/src/main/java/kr/dgucaps/capsv4/repository/EventRepository.java
@@ -3,13 +3,17 @@ package kr.dgucaps.capsv4.repository;
 import kr.dgucaps.capsv4.entity.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
 
-//    Page<Event> findAll(Pageable pageable);
+    @Override
+    @EntityGraph(attributePaths = {"user"})
+    Page<Event> findAll(Pageable pageable);
 
+    @EntityGraph(attributePaths = {"user"})
     Page<Event> findByTitleContaining(String title, Pageable pageable);
 }
