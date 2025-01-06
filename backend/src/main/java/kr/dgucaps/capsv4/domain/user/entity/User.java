4c2a70e4966da1838a24d3a6ba433668469c80fa
Jinsoo Yoon <floreo1242@gmail.com>
2025-01-06 11:44:47 +0900

    Refactor: Book 도메인 관련 코드 위치 수정

--- backend/src/main/java/kr/dgucaps/capsv4/domain/user/entity/User.java
+++ backend/src/main/java/kr/dgucaps/capsv4/domain/user/entity/User.java
@@ -3,6 +3,7 @@ package kr.dgucaps.capsv4.domain.user.entity;
 import jakarta.persistence.*;
 import kr.dgucaps.capsv4.domain.board.entity.Board;
 import kr.dgucaps.capsv4.domain.board.entity.BoardLike;
+import kr.dgucaps.capsv4.domain.book.entity.Checkout;
 import kr.dgucaps.capsv4.domain.comment.entity.Comment;
 import kr.dgucaps.capsv4.domain.event.entity.Event;
 import kr.dgucaps.capsv4.domain.event.entity.EventQuizApply;
